c01c9687624b381bb9cefa698323520a63b2e5d5
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2015-11-07 23:11:49 +0000

    Removed unused comment

--- functions/gd.fish
+++ functions/gd.fish
@@ -3,6 +3,5 @@ function gd
 	set script_dir {$parent_directory}/scripts/
     # echo $script_dir # Debug
 	git diff --color | diff-highlight | _strip_diff_leading_symbols
-	# git diff --color | diff-highlight 
 end
 
