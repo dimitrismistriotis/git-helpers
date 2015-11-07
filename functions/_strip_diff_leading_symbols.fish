d5adf2cbd5bc7e23315a0543f898440a0ac9e290
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2015-11-07 17:44:58 +0000

    display ruler

--- functions/_strip_diff_leading_symbols.fish
+++ functions/_strip_diff_leading_symbols.fish
@@ -1,9 +1,10 @@
 function _strip_diff_leading_symbols
     set color_code_regex "(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K])"
     echo $color_code_regex
+    set ruler_output (_ruler)
 
     sed -r "s/^($color_code_regex)diff --git .*"'$'"//g" | \
-        sed -r "s/^($color_code_regex)index .*"'$'"/\n\1"'$'"(_rule)/g" | \
-        sed -r "s/^($color_code_regex)\+\+\+(.*)"'$'"/\1+++\5\n\1"'$'"(_rule)\x1B\[m/g" |\
+        sed -r "s/^($color_code_regex)index .*"'$'"/\n\1"{$ruler_output}"/g" | \
+        sed -r "s/^($color_code_regex)\+\+\+(.*)"'$'"/\1+++\5\n\1"{$ruler_output}"\x1B\[m/g" |\
         sed -r "s/^($color_code_regex)[\+\-]/\1 /g"
 end
