3fecf2d1fc849f8df2df11dce0c6a19ac2831f25
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2015-11-07 23:16:47 +0000

    _git_helpers_strip_diff_leading_symbols rename

--- functions/_git_helpers_strip_diff_leading_symbols.fish
+++ functions/_git_helpers_strip_diff_leading_symbols.fish
@@ -1,4 +1,4 @@
-function _strip_diff_leading_symbols
+function _git_helpers_strip_diff_leading_symbols
     set color_code_regex "(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K])"
     # echo $color_code_regex # DEBUG
     set ruler_output (_git_helpers_ruler)
