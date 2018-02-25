deaff3a939d03e40ee2933d85ca4a3a0b714e856
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2018-02-25 10:38:32 +0000

    eval

--- functions/gd.fish
+++ functions/gd.fish
@@ -2,6 +2,6 @@ function gd
 	set parent_directory (dirname (dirname (status --current-filename)))
 	set script_dir {$parent_directory}/scripts/
     # echo $script_dir # Debug
-	git diff --color | ${script_dir}/diff-so-fancy
+	git diff --color | eval ${script_dir}/diff-so-fancy
 end
 
