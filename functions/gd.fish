1fa27f0482383f0d61d050302cea88fa56f0bf48
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2018-02-25 10:35:34 +0000

    Substitute with diff-so-fancy

--- functions/gd.fish
+++ functions/gd.fish
@@ -2,6 +2,6 @@ function gd
 	set parent_directory (dirname (dirname (status --current-filename)))
 	set script_dir {$parent_directory}/scripts/
     # echo $script_dir # Debug
-	git diff --color | diff-highlight | _git_helpers_strip_diff_leading_symbols
+	git diff --color | ${script_dir}/diff-so-fancy
 end
 
