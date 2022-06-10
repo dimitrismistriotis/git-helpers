fad8e0e6f65c12463345b4e5e232fa8963242568
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2022-06-10 12:19:11 +0100

    Extend to main

--- functions/delete_merged_branches.fish
+++ functions/delete_merged_branches.fish
@@ -1,4 +1,5 @@
 # http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged
 function delete_merged_branches
-	git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d
+	# All merged ones   | Not master, main, *         | Remove branch
+	git branch --merged | grep -v -E "master|main|\*" | xargs -n 1 git branch -d
 end
