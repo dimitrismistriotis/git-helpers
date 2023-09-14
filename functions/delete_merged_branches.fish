c58f59e690df30f1f759d769a6e04401462dba0a
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2023-09-14 09:40:44 +0100

    Stop execution when there are no branches to merge

--- functions/delete_merged_branches.fish
+++ functions/delete_merged_branches.fish
@@ -1,5 +1,19 @@
 # http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged
 function delete_merged_branches
+  #
+  # Existing merged branches
+  #
+  #                      All merged ones   | Not master, main, *
+  set merged_branches (git branch --merged | grep -v -E "master|main|\*")
+  echo "merged_branches: $merged_branches"
+  if [ (count $merged_branches) -eq 0 ]
+    echo "No branches to merge"
+    return 0
+  end
+
+  #
+  # Need to use variable instead of recalculating it
+  #
 	# All merged ones   | Not master, main, *         | Remove branch
 	git branch --merged | grep -v -E "master|main|\*" | xargs -n 1 git branch -d
 end
